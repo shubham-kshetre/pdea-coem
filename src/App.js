8c6b80238b55de9e1a72380cb151a8b47b090eb2
Sayuri <sayurikamble123@gmail.com>
2024-04-12 23:53:54 +0530

    MUI setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>PDEA COEM</p>
+        <Button variant="contained">Hello College</Button>
       </header>
     </div>
   );
