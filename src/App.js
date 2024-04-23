1f6c56f5cbd50427d9cf957aa8f273d5a99725c8
Sayuri <sayurikamble123@gmail.com>
2024-04-23 17:19:32 +0530

    cleaning unwanted code.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import "./App.css";
 import { Box } from "@mui/material";
 import HeroSection from "./Components/HeroSection/HereSection";
 import Statistics from "./Components/Statistics/Statistics";
 import About from "./Components/AboutSection/About";
-// import FacultyContact from "./Components/ContactSection/FacultyContact";
-
 
 const LandingPage = () => {
   return (
     <Box className="parent">
       <HeroSection />
+
+      <About />
+
       <Statistics />
-      <About/>
-      {/* <FacultyContact/> */}
     </Box>
   );
 };
