f01c3ece83d7f598ba3586ada6b1501275641029
Akshay Kalushe <akshaykalushe35@gmail.com>
2024-04-23 17:29:34 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 import { Box } from "@mui/material";
 import HeroSection from "./Components/HeroSection/HereSection";
 import About from "./Components/AboutSection/About";
-// import FacultyContact from "./Components/ContactSection/FacultyContact";
+import EventCarousel from "./Components/Carousel/EventCarousel";
+import FacultyContact from "./Components/ContactSection/FacultyContact";
 
 const LandingPage = () => {
   return (
     <Box className="parent">
       <HeroSection />
       <About/>
-      {/* <FacultyContact/> */}
+      <FacultyContact/>
+      <EventCarousel/>
     </Box>
   );
 };
