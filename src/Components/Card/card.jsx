7f5dac8c9c72597935531b3214a2f1544a06dee1
Sayuri <sayurikamble123@gmail.com>
2024-05-01 23:27:05 +0530

    fix: card error

--- src/Components/Card/card.jsx
+++ src/Components/Card/card.jsx
@@ -4,7 +4,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActions } from '@mui/material';
-import "./card.css";
 import { Box } from "@mui/material";
 
 function CardComponent() {
